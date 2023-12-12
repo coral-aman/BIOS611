b18708eb54063e339293affda94c52167739bd7b
coral23 <coral23@bios611-09.bios.unc.edu>
2023-12-12 11:38:44 -0500

    subset NFL offensive dataset to 2022 games only

---
+++ read_in_data.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+library(readr)
+nfl_offensive_stats <- read_csv("source_data/nfl_offensive_stats.csv")
+
+nfl_offensive_stats$game_id <- substr(nfl_offensive_stats$game_id, 1, 4)
+subset_nfl_offensive_stats <- nfl_offensive_stats[nfl_offensive_stats$game_id == "2022", ]
+subset_nfl_offensive_stats
+
+write.csv(subset_nfl_offensive_stats, "derived_data/nfl_offensive_stats_subset.csv", row.names = FALSE)
+
+
