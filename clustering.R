063c963a62ea90f54e1224b73e135a44e8fca017
coral23 <coral23@bios611-09.bios.unc.edu>
2023-12-14 01:32:00 -0500

    Added figures to display kmeans results.

--- clustering.R
+++ clustering.R
@@ -1,27 +1,19 @@
 library(tidyverse)
+numeric_data <- read_csv("derived_data/numeric_offensivestats_subset.csv")
 
-nfl_offensive_stats <- read_csv("work/source_data/nfl_offensive_stats.csv", col_names = T)
+##Clustering##
 
-numeric_data <- nfl_offensive_stats[, sapply(nfl_offensive_stats, is.numeric)]
+kmeans_result <- kmeans(numeric_data, centers = 3)
 
-pca_result <- prcomp(numeric_data)
-summary(pca_result)
-
-variance <- (pca_result$sdev)^2
-cumulative_variance <- cumsum(variance) / sum(variance)
-
-plot <- data.frame(Dimension =1:length(cumulative_variance), CumulativeVariance = cumulative_variance)
-
-ggplot(plot, aes(x= Dimension, y = CumulativeVariance)) + 
-  geom_line() + 
-  geom_hline(yintercept = 0.85) +
-  xlab("Principal Component") + 
-  ylab("Cumulative Proportion of Variance Explained") +
-  ggtitle("PCA cumulative Variance Plot")
-ggsave("work/figures/explained_variance.png");
+ggplot(numeric_data, aes(x = pass_yds)) +
+  geom_density(aes(fill = factor(kmeans_result$cluster))) +
+  labs(title = "K-means Clustering") + xlim(0, 10)
+ggsave("work/figures/Clustering_PassingYards.png")
 
 
-
-
+ggplot(numeric_data, aes(x = pass_tds)) +
+  geom_density(aes(fill = factor(kmeans_result$cluster))) +
+  labs(title = "K-means Clustering") + xlim(0, 40) + ylim(0, 3.0)
+ggsave("work/figures/Clustering_PassingTouchdowns.png")
 
 
