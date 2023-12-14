3c95ccf06f5569f8469da57f0a2756425611b5c9
coral23 <coral23@bios611-09.bios.unc.edu>
2023-12-14 01:32:47 -0500

    Initial commit for Clustering Results with Rushing Yards

---
+++ Clustering_RushYds.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+numeric_data <- read_csv("work/derived_data/numeric_offensivestats_subset.csv")
+
+##Clustering##
+
+kmeans_result <- kmeans(numeric_data, centers = 3)
+
+ggplot(numeric_data, aes(x = rush_yds)) +
+  geom_density(aes(fill = factor(kmeans_result$cluster))) + theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "K-means Clustering for Rushing Yards", x="Rushing Yards", y="Density") + xlim(0, 50.0) + ylim(0, 0.20)
+ggsave("work/figures/Clustering_PassingTouchdowns.png")
