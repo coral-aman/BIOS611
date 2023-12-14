fa7052a3a72563afee10016cf6b58c07df17dd94
coral23 <coral23@bios611-09.bios.unc.edu>
2023-12-14 01:10:53 -0500

    Modified Variance Figure Aesthetics

--- Dimensionality_Reduction.R
+++ Dimensionality_Reduction.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-numeric_data <- read_csv("derived_data/numeric_offensivestats_subset.csv")
+numeric_data <- read_csv("derived_data/numeric_offensivestats_subset.csv", col_names = T)
 pca_result <- prcomp(numeric_data)
 summary(pca_result)
 variance <- (pca_result$sdev)^2
@@ -7,17 +7,11 @@ cumulative_variance <- cumsum(variance) / sum(variance)
 plot <- data.frame(Dimension =1:length(cumulative_variance), CumulativeVariance = cumulative_variance)
 
 ggplot(plot, aes(x= Dimension, y = CumulativeVariance)) + 
-  geom_line() + 
-  geom_hline(yintercept = 0.85) +
+  geom_line(col = "blue") + 
+  geom_hline(yintercept = 0.85) + theme(plot.title = element_text(hjust = 0.5, size = 10), axis.title.x = element_text(size = 6), axis.title.y = element_text(size = 6), 
+  axis.text.x = element_text(size = 6), axis.text.y = element_text(size = 6)) +
   xlab("Principal Component") + 
   ylab("Cumulative Proportion of Variance Explained") +
   ggtitle("PCA Cumulative Variance Plot")
-ggsave("work/figures/NFL_explained_variance.png");
-
-
-pcs <- pca_result$x[,1:56] %>% as_tibble(name_repair="simple")
-ggplot(pcs, aes(PC1, PC2)) + geom_point() + xlim(-50, 50)
-ggsave("work/figures/NFL_principle_components.png");
-ggplot(pcs, aes(PC1, PC2)) + geom_point(aes(color=numeric_data$pass_yds)) + xlim(-50,50) + ylim(0, 200)
-
+ggsave("figures/NFL_explained_variance.png")
 
