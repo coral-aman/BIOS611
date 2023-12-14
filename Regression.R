5000b54f4325d69fd1762cf49d78818dd239c531
coral23 <coral23@bios611-09.bios.unc.edu>
2023-12-14 00:20:33 -0500

    Added figure of Regression Model

--- Regression.R
+++ Regression.R
@@ -2,23 +2,13 @@ library(tidyverse)
 
 nfl_offensive_stats <- read_csv("work/derived_data/nfl_offensive_stats_subset.csv")
 
-pass_yds <- nfl_offensive_stats %>%
-  group_by(team) %>%
-  summarise(Total_PassYds = sum(pass_yds)) %>%
-  arrange(desc(Total_PassYds))
-
-pass_tds<- nfl_offensive_stats %>%
-  group_by(team) %>%
-  summarise(Total_PassTd = sum(pass_td)) %>%
-  arrange(desc(Total_PassTd))
-
 team_stats <- nfl_offensive_stats %>%
   group_by(team) %>%
   summarise(total_passing_yards = sum(pass_yds),
             total_passing_touchdowns = sum(pass_td))
 
-# Step 2: Perform Simple Linear Regression
 regression_model <- lm(total_passing_touchdowns ~ total_passing_yards, data = team_stats)
+summary(regression_model)
 
 ggplot(team_stats, aes(x = total_passing_yards, y = total_passing_touchdowns, label=team)) +
   geom_point(size = 1, col= "blue")+
@@ -33,9 +23,9 @@ ggplot(team_stats, aes(x = total_passing_yards, y = total_passing_touchdowns, la
   theme(plot.title = element_text(hjust = 0.5))  + theme(plot.subtitle = element_text(hjust = 0.5)) + 
   scale_x_continuous(breaks = seq(0, max(team_stats$total_passing_yards), by = 250)) + 
   scale_y_continuous(breaks = seq(0, 25, by = 5))
+ggsave("figures/regression_PassTd~PassYds.png", plot = p)
 
-# Print the regression summary
-summary(regression_model)
+
 
 
 
