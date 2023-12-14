0b0df2bc59c3a91d3aaaac407e775a4971a5484b
coral23 <coral23@bios611-09.bios.unc.edu>
2023-12-14 16:19:38 -0500

    Adjusted axis range for figure.

--- Clustering_RushYds.R
+++ Clustering_RushYds.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-numeric_data <- read_csv("work/derived_data/numeric_offensivestats_subset.csv")
+numeric_data <- read_csv("derived_data/numeric_offensivestats_subset.csv")
 
 ##Clustering##
 
@@ -8,4 +8,4 @@ kmeans_result <- kmeans(numeric_data, centers = 3)
 ggplot(numeric_data, aes(x = rush_yds)) +
   geom_density(aes(fill = factor(kmeans_result$cluster))) + theme(plot.title = element_text(hjust = 0.5)) +
   labs(title = "K-means Clustering for Rushing Yards", x="Rushing Yards", y="Density") + xlim(0, 50.0) + ylim(0, 0.20)
-ggsave("work/figures/Clustering_PassingTouchdowns.png")
+ggsave("figures/Clustering_RushingYards.png")
