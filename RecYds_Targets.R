97b72194667f8285f91d94f88bc488092c73f96a
coral23 <coral23@bios611-09.bios.unc.edu>
2023-12-14 12:29:46 -0500

    Made edits to scatterplot.

--- RecYds_Targets.R
+++ RecYds_Targets.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-nfl_offensive_stats <- read_csv("work/source_data/nfl_offensive_stats.csv")
+nfl_offensive_stats <- read_csv("source_data/nfl_offensive_stats.csv")
 
 nfl_offensive_stats$position <- as.character(nfl_offensive_stats$position)
 
@@ -10,7 +10,12 @@ ggplot(subset_wrs, aes(x= targets, y= rec_yds)) +
 geom_point(color = "blue",  
            size = .5) +
   labs(
-    title = "Total Receiving Yards for NFL Wide Receivers",
+    title = "Total Receiving Yards for NFL WRs",
     x = "Targets",
     y = "Total Receiving Yards"
-  ) + ylim(0, 150) + geom_smooth(method = "lm", se = FALSE, color = "orange") 
+  ) + ylim(0, 150) + geom_smooth(method = "lm", se = FALSE, color = "orange") +
+  theme(plot.title = element_text(hjust = 0.5))  + theme(plot.title = element_text(size = 6), 
+    axis.title.x = element_text(size=6), axis.title.y = element_text(size=6), 
+    axis.text.y = element_text(size = 6), axis.text.x = element_text(size=6))
+
+ggsave("figures/RecYds_Targets.png")
