9423a0d87b79c9db75434c39121c9a93c09dda8d
coral23 <coral23@bios611-09.bios.unc.edu>
2023-12-14 16:17:10 -0500

    Made adjustments to figure axis scales.

--- clustering.R
+++ clustering.R
@@ -6,14 +6,13 @@ numeric_data <- read_csv("derived_data/numeric_offensivestats_subset.csv")
 kmeans_result <- kmeans(numeric_data, centers = 3)
 
 ggplot(numeric_data, aes(x = pass_yds)) +
-  geom_density(aes(fill = factor(kmeans_result$cluster))) +
-  labs(title = "K-means Clustering") + xlim(0, 10)
-ggsave("work/figures/Clustering_PassingYards.png")
+  geom_density(aes(fill = factor(kmeans_result$cluster))) + theme(plot.title = element_text(hjust = 0.5)) + 
+  labs(title = "K-means Clustering for Passing Yards", x="Passing Yards", y="
+Density") + xlim(0, 50.0) + ylim(0, 0.03) 
+  
+ggsave("figures/Clustering_PassingYards.png")
 
 
-ggplot(numeric_data, aes(x = pass_tds)) +
-  geom_density(aes(fill = factor(kmeans_result$cluster))) +
-  labs(title = "K-means Clustering") + xlim(0, 40) + ylim(0, 3.0)
-ggsave("work/figures/Clustering_PassingTouchdowns.png")
+
 
 
